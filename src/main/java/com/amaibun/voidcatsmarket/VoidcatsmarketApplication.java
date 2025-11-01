49744d8b8b303e7e07b4cd15c425932e0dd151f6
Amaibun <amaibun@arch.localdomain>
2025-11-01 18:55:22 +0100

    Initial commit.

---
+++ src/main/java/com/amaibun/voidcatsmarket/VoidcatsmarketApplication.java
@@ -0,0 +1,13 @@
+package com.amaibun.voidcatsmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoidcatsmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoidcatsmarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amaibun/voidcatsmarket/VoidcatsmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amaibun.voidcatsmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoidcatsmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
