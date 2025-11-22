7503205543f15e6e8b3b90a7e69d577e6c238ab8
Amaibun <amaibun@arch.localdomain>
2025-11-22 15:19:25 +0100

    Changes return types of each mapping to ResponseEntity. Adds a version to the root mapping url

--- src/main/java/com/amaibun/voidcatsmarket/controllers/ProductController.java
+++ src/main/java/com/amaibun/voidcatsmarket/controllers/ProductController.java
@@ -3,6 +3,8 @@ package com.amaibun.voidcatsmarket.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,35 +17,43 @@ import org.springframework.web.bind.annotation.RestController;
 import com.amaibun.voidcatsmarket.dtos.ProductDTO;
 import com.amaibun.voidcatsmarket.services.ProductService;
 
+import jakarta.validation.Valid;
+
 
 @RestController
-@RequestMapping("/api/products")
+@RequestMapping("/api/v1/products")
 public class ProductController {
     @Autowired
     ProductService productService;
 
     @GetMapping
-    public List<ProductDTO> getAllProducts() {
-        return productService.getAllProducts();
+    public ResponseEntity<List<ProductDTO>> getAllProducts() {
+        List<ProductDTO> productDtos = productService.getAllProducts();
+
+        return ResponseEntity.ok(productDtos);
     }
 
     @GetMapping("/{productId}")
-    public ProductDTO getProductById(@PathVariable Long productId) {
-        return productService.getProduct(productId);
+    public ResponseEntity<ProductDTO> getProductById(@PathVariable Long productId) {
+        ProductDTO foundProductDto = productService.getProduct(productId);
+        return ResponseEntity.ok(foundProductDto);
     }
 
     @PostMapping
-    public ProductDTO createProduct(@RequestBody ProductDTO product) {
-        return productService.createProduct(product);
+    public ResponseEntity<ProductDTO> createProduct(@RequestBody @Valid ProductDTO productDto) {
+        ProductDTO createdProductDTO = productService.createProduct(productDto);
+        return new ResponseEntity<>(createdProductDTO, HttpStatus.CREATED);
     }
 
     @PutMapping("/{productId}")
-    public ProductDTO updateProduct(@PathVariable Long productId, @RequestBody ProductDTO product) {
-        return productService.updateProduct(productId, product);
+    public ResponseEntity<ProductDTO> updateProduct(@PathVariable Long productId, @RequestBody @Valid ProductDTO productDto) {
+        ProductDTO updatedProductDto = productService.updateProduct(productId, productDto);
+        return ResponseEntity.ok(updatedProductDto);
     }
 
     @DeleteMapping("/{productId}")
-    public void deleteProduct(@PathVariable Long productId) {
+    public ResponseEntity<ProductDTO> deleteProduct(@PathVariable Long productId) {
         productService.deleteProduct(productId);
+        return ResponseEntity.ok(null);
     }
 }
