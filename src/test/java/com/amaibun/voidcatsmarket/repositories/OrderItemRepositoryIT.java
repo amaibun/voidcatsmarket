e8286f476358f991af82e436b40f8bf0d6b914c5
Amaibun <>
2026-01-08 20:03:04 +0100

    Adds tests for repository.

---
+++ src/test/java/com/amaibun/voidcatsmarket/repositories/OrderItemRepositoryIT.java
@@ -0,0 +1,46 @@
+package com.amaibun.voidcatsmarket.repositories;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import com.amaibun.voidcatsmarket.repositories.projections.ProductSalesReport;
+
+@Testcontainers
+@SpringBootTest
+@ActiveProfiles("testdb")
+class OrderItemRepositoryIT {
+
+    @Container
+    static PostgreSQLContainer<?> postgres =
+        new PostgreSQLContainer<>("postgres:15");
+
+    @DynamicPropertySource
+    static void props(DynamicPropertyRegistry r) {
+        r.add("spring.datasource.url", postgres::getJdbcUrl);
+        r.add("spring.datasource.username", postgres::getUsername);
+        r.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @Autowired
+    OrderItemRepository repository;
+
+    @Test
+    void shouldReturnProductSalesReport() {
+        List<ProductSalesReport> report =
+            repository.findMostPurchasedProducts();
+
+        assertNotNull(report);
+    }
+}
+
