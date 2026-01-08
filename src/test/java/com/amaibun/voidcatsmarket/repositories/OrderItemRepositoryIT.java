bf2c0b6bf06011a73bff091305b60d981243f52f
Amaibun <>
2026-01-08 20:47:42 +0100

    Fixes OrderItemRepositoryIT, which got terribly transfigured for some reason.

--- src/test/java/com/amaibun/voidcatsmarket/repositories/OrderItemRepositoryIT.java
+++ src/test/java/com/amaibun/voidcatsmarket/repositories/OrderItemRepositoryIT.java
@@ -1,9 +1,8 @@
 package com.amaibun.voidcatsmarket.repositories;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
