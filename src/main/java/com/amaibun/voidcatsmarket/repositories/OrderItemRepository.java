e24a65d5caaf6e91bb1bb403009db96ee68995f5
Amaibun <>
2026-01-10 14:06:09 +0100

    A change for future commits slipped into this branch and broke everything. This commit fixes OrderItemRepository.

--- src/main/java/com/amaibun/voidcatsmarket/repositories/OrderItemRepository.java
+++ src/main/java/com/amaibun/voidcatsmarket/repositories/OrderItemRepository.java
@@ -1,27 +1,9 @@
 package com.amaibun.voidcatsmarket.repositories;
 
-
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.amaibun.voidcatsmarket.models.OrderItem;
-import com.amaibun.voidcatsmarket.repositories.projections.ProductSalesReport;
 
 @Repository
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-
-    @Query("""
-        SELECT 
-            p.title AS title,
-            SUM(oi.quantity) AS totalSold
-        FROM OrderItem oi
-        JOIN oi.product p
-        GROUP BY p.title
-        ORDER BY totalSold DESC
-    """)
-    public List<ProductSalesReport> findMostPurchasedProducts();
-}
-
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {}
